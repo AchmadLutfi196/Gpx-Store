876fbd5691f1f4037cede54b6fb530dd74f0b7a5
AchmadLutfi196 <achmadlutfi196@gmail.com>
2025-05-17 17:37:48 +0700

    up reviews

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -5,41 +5,27 @@ namespace Database\Seeders;
 use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class CategorySeeder extends Seeder
 {
     public function run(): void
     {
         $categories = [
-            [
-                'name' => 'Sepatu Lari',
-                // Sesuaikan dengan kolom yang ada di tabel categories
-                // Hapus kolom yang tidak ada
-            ],
-            [
-                'name' => 'Sepatu Basket',
-                // Sesuaikan dengan kolom yang ada
-            ],
-            [
-                'name' => 'Sepatu Casual',
-                // Sesuaikan dengan kolom yang ada
-            ],
-            [
-                'name' => 'Pakaian Pria',
-                // Sesuaikan dengan kolom yang ada
-            ],
-            [
-                'name' => 'Pakaian Wanita',
-                // Sesuaikan dengan kolom yang ada
-            ],
-            [
-                'name' => 'Aksesoris',
-                // Sesuaikan dengan kolom yang ada
-            ],
+            'Ransel',
+            'Tas Selempang',
+            'Tas Jinjing',
+            'Tas Olahraga',
+            'Koper',
+            'Tas Laptop',
+            'Tas Premium',
         ];
 
         foreach ($categories as $category) {
-            Category::create($category);
+            Category::create([
+                'name' => $category,
+                'slug' => Str::slug($category),
+            ]);
         }
     }
 }
